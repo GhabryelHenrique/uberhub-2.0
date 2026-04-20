0630f1d5fb802f61332415cfd5b39accbd37c7ed
Ghabryel <ghabryelhenrique@hotmail.com>
2026-04-20 11:19:33 -0300

    Update API keys in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: 'AIzaSyDUHGGWwcM5VZJc2NqGoukmP4F7z7Uu6A8',
-  GEMINI_API_KEY: "AIzaSyDNWcP4Cnyk6v9Fiyq8VpR39cnWPmHUqfg",
+  googleMapsApiKey: 'GEMINI_API_KEY',
+  GEMINI_API_KEY: "GEMINI_API_KEY",
   apiVersion: 'v1'
 };
